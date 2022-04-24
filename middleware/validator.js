488d8384cdb66845b2bb62fc63e7bc0a8fb8c3ba
Tobechi <tobechichukwuleta125@gmail.com>
2022-04-24 20:07:21 +0100

    validator update

--- middleware/validator.js
+++ middleware/validator.js
@@ -1,15 +1,15 @@
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
-const response = require("../utils/libs/response");
 dotenv.config();
+const response = require("../utils/libs/response"); 
 
 const validateAuth = async (req, res, next) => {
-  const token = req.get("Authorization");
-  // const token = req.headers.authorization;
+  // const token = req.get("Authorization");
+  const token = req.headers.authorization.split(' ')[1];
   if (token) {
 
     jwt.verify(
-      token.split(" ")[1],
+      token,
       `${process.env.ZIGARA_ACCESS_TOKEN_SECRET}`,
       async (err, decoded) => {
         console.log(err)
